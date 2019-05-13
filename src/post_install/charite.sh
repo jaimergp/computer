42d87e744bafaf141ce2ea85f914fe02c17e4701
Jaime RGP <jaime.rogue@gmail.com>
2019-05-13 18:06:50 +0200

    Some fixes in the Charité config

--- src/post_install/charite.sh
+++ src/post_install/charite.sh
@@ -46,21 +46,43 @@ if [ ! -d "$HOME/mnt/charite_s" ]; then
     mkdir -p ~/mnt/charite_{t,s}
 fi
 fi
-print_in_purple "   T and S drive default configurations have been added."
-print_in_purple "   Edit your credentials at this file: ~/.smbcredentials"
-print_in_purple "   Then, uncomment and fill details to enable access: /etc/fstab"
+print_in_purple "    T and S drive default configurations have been added."
+print_in_purple "    Edit your credentials at this file: ~/.smbcredentials"
+print_in_purple "    Then, uncomment and fill details to enable access: /etc/fstab"
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 print_in_purple "\n   Installing and patching VMWare Horizon\n\n"
 
-wget https://download3.vmware.com/software/view/viewclients/CART19FQ4/VMware-Horizon-Client-4.10.0-11053294.x64.bundle -O vmware.bundle
+execute "wget https://download3.vmware.com/software/view/viewclients/CART19FQ4/VMware-Horizon-Client-4.10.0-11053294.x64.bundle -O vmware.bundle" \
+    "Downloading"
 chmod +x vmware.bundle
-sudo ./vmware.bundle --console --required
+execute "sudo ./vmware.bundle --console --required" \
+    "Installing"
 sudo cp /usr/share/applications/vmware-view.desktop /usr/share/applications/vmware-view-proxy.desktop
 sudo sed -i 's/Exec=vmware-view/Exec=env http_proxy="" https_proxy="" vmware-view/;s/VMware Horizon Client/No-Proxy VMware Horizon Client/g' \
           /usr/share/applications/vmware-view-proxy.desktop
 rm vmware.bundle
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Adding SSH pre-configuration (check ~/.ssh/config.d)\n\n"
+
+cat << EOF > ~/.ssh/config.d/charite
+Host bihlogin1
+    Hostname med-login1.bihealth.org
+    # User rodriguj_c
+
+Host bihlogin
+    Hostname med-login2.bihealth.org
+    # User rodriguj_c
+
+Host bihtransfer
+    Hostname med-transfer1.bihealth.org
+    # User rodriguj_c
+
+Host bihtransfer2
+    Hostname med-transfer2.bihealth.org
+    # User rodriguj_c
+EOF
