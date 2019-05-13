c3734ab4c65fbff38f55c0a8066eb836fda8d68f
Jaime RGP <jaime.rogue@gmail.com>
2019-05-13 18:07:06 +0200

    Less verbose conda post-install with sudo

--- src/post_install/conda.sh
+++ src/post_install/conda.sh
@@ -3,8 +3,13 @@
 cd "$(dirname "${BASH_SOURCE[0]}")" \
     && . "../utils.sh"
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Configuring conda\n\n"
+
 . /opt/miniconda/etc/profile.d/conda.sh
 
-conda activate
-conda update -q -y --all
-conda install -q -y jupyterlab matplotlib pandas numpy scipy
+execute "sudo conda activate" "Activate base environment"
+execute "sudo conda update -q -y --all" "Update base packages"
+execute "sudo conda install -q -y jupyterlab matplotlib pandas numpy scipy" \
+    "Install PyData packages"
